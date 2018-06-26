41b2ce1a7f783a0df1c267bef8470d25c7eca0bf
powco-ih-you <ih.you@powderroom.co.kr>
2018-06-26 16:58:14 +0900

    crud

--- index.ts
+++ index.ts
@@ -1,11 +1,42 @@
-import elasticsearch = require('elasticsearch')
+import elasticsearch = require('elasticsearch');
 
 const start = async () => {
     const client = new elasticsearch.Client({
-        host: 'localhost:9200',
-    })
-    await client.ping({requestTimeout: 300000})
-    console.log('pinged server')
-}
+        host: 'localhost:9200'
+    });
+    await client.ping({ requestTimeout: 300000 });
+    console.log('pinged server');
+    const query = 'London';
+    const resp = await client.search({
+        index: 'osm',
+        type: 'place',
+        body: {
+            sort: [
+                {
+                    place_rank_num: { order: 'desc' },
+                    importance_num: { order: 'desc' }
+                }
+            ],
+            query: {
+                bool: {
+                    should: [
+                        {
+                            match: {
+                                name: query
+                            }
+                        },
+                        {
+                            match: {
+                                alternative_name: query
+                            }
+                        }
+                    ]
+                }
+            }
+        }
+    });
+    const { hits } = resp.hits;
+    console.log(hits);
+};
 
-start()
+start();
